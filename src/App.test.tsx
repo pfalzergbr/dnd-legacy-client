d49387da1fb2b8cf7f8496d89264b0da2114c688
Gabor Pfalzer <pfalzer@gmail.com>
2021-05-08 07:25:42 +0100

    clear out boilerplate, display dummy app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders dummy app component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloElement = screen.getByText(/hello dungeon/i);
+  expect(helloElement).toBeInTheDocument();
 });
