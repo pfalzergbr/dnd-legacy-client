c14cb4806fdf97e9dfe749cd27bce52ed7842987
Gabor Pfalzer <pfalzer@gmail.com>
2021-05-14 08:39:43 +0100

    add http link to apollo client

--- src/Services/client.tsx
+++ src/Services/client.tsx
@@ -1,22 +1,12 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-// import { onError } from '@apollo/client/link/error';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-// const errorLink = onError(({ graphQLErrors, networkError }) => {
-//   if (graphQLErrors) {
-//     graphQLErrors.map(({message}) => {
-//       alert(`GraphQL error ${message}`)
-//     })
-//   }
-// });
-
-// const link = from([
-//   errorLink,
-//   new HttpLink({ uri: 'http://localhost:8080/graphql' }),
-// ]);
+const link = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_API,
+  credentials: 'include',
+})
 
 export const client = new ApolloClient({
-  // link: link,
-  uri: process.env.REACT_APP_GRAPHQL_API,
+  link,
   cache: new InMemoryCache(),
 });
 
