09dab5727a5b19c1db35932fa319c0ae12316cf4
Gabor Pfalzer <pfalzer@gmail.com>
2021-05-14 15:57:28 +0100

    add no-cors to client fetch options

--- src/Services/client.tsx
+++ src/Services/client.tsx
@@ -3,6 +3,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 const link = new HttpLink({
   uri: process.env.REACT_APP_GRAPHQL_API,
   credentials: 'include',
+  fetchOptions: {
+    mode: 'no-cors'
+  }
 })
 
 export const client = new ApolloClient({
