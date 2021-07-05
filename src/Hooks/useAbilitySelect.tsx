b5701210b0f5668658b5e3fb6f270f6658587745
Gabor Pfalzer <pfalzer@gmail.com>
2021-07-05 11:38:55 +0100

    change ability base to 10

--- src/Hooks/useAbilitySelect.tsx
+++ src/Hooks/useAbilitySelect.tsx
@@ -30,12 +30,12 @@ const initialState: AbilityState = {
   //   remaining: 0
   // },
   abilities: {
-    strength: 0,
-    dexterity: 0,
-    constitution: 0,
-    intelligence: 0,
-    wisdom: 0,
-    charisma: 0
+    strength: 10,
+    dexterity: 10,
+    constitution: 10,
+    intelligence: 10,
+    wisdom: 10,
+    charisma: 10
   }
 }
 
