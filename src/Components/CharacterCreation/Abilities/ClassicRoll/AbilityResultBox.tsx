a0e9348f7ac81654ff1c684df7b0e310dd8c1ed1
Gabor Pfalzer <pfalzer@gmail.com>
2021-07-07 10:03:00 +0100

    rework AbilityResultBox

--- src/Components/CharacterCreation/Abilities/ClassicRoll/AbilityResultBox.tsx
+++ src/Components/CharacterCreation/Abilities/ClassicRoll/AbilityResultBox.tsx
@@ -1,5 +1,5 @@
-import { useDrag } from 'react-dnd';
-import { itemTypes } from '../../../../Utils/itemTypes'
+import AbilityDropContainer from '../../../UI/DragDrop/AbilityDropContainer';
+import AbilityDrag from '../../../UI/DragDrop/AbilityDrag';
 
 export interface AbilityResultBoxProps {
   result: number;
@@ -7,35 +7,9 @@ export interface AbilityResultBoxProps {
 }
 
 const AbilityResultBox: React.FC<AbilityResultBoxProps> = ({ result }) => {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: itemTypes.ABILITY_BOX,
-    item: {result},
-    collect: (monitor) => ({
-      result,
-      isDragging: monitor.isDragging(),
-    }),
-    // end: (item: any, monitor) => {
-    //   const dropResult = monitor.getDropResult()
-    //   console.log(item, dropResult)
-    // }
-  }));
-
   return (
-    <div
-      ref={drag}
-      style={{
-        display: 'flex',
-        width: '50px',
-        height: '50px',
-        margin: '.5rem',
-        marginBottom: '1rem',
-        alignItems: 'center',
-        justifyContent: 'center',
-        border: '1px solid black',
-        opacity: isDragging ? '0.5' : '1'
-      }}
-    >
-      {result}
+    <div>
+      {result ? <AbilityDrag value={result} /> : <AbilityDropContainer />}
     </div>
   );
 };
